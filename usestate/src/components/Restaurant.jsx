74ec9274fbb1ee94d4306b19986c1bfd96e1e017
LauraV702 <dsfjsdfjlsdk@student.greenriver.edu>
2025-04-22 11:40:41 -0700

    Deliverable 2, Added OnClick Events

--- usestate/src/components/Restaurant.jsx
+++ usestate/src/components/Restaurant.jsx
@@ -1,4 +1,15 @@
+import { useState } from "react";
 function Restaurant(props) {
+  const [hours, setHours] = useState(false);
+  const [menu, setMenu] = useState(false);
+
+  const handleHours = () => {
+    setHours((prev) => !prev);
+  };
+  const handleMenu = () => {
+    setMenu((prev) => !prev);
+  };
+
   return (
     <div className="restaurant">
       <img src={props.restaurant.image} alt={props.name} />
@@ -9,10 +20,12 @@ function Restaurant(props) {
         <li>{props.restaurant.cuisine}</li>
         <li>{props.restaurant.rating}</li>
       </ul>
-      <button>Hours</button>
-      <button>Menu</button>
+      <button onClick={handleHours}>Hours</button>
+      <button onClick={handleMenu}>Menu</button>
+
     </div>
   );
 }
 
 export default Restaurant;
+
