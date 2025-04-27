87281a00de2eaf5d5ff4eb0e1ee651722e499c91
LauraV702 <dsfjsdfjlsdk@student.greenriver.edu>
2025-04-27 13:16:56 -0700

    Added comments for clarity

--- usestate/src/components/Restaurant.jsx
+++ usestate/src/components/Restaurant.jsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
 
 function Restaurant(props) {
+  //visibility of hours and menu
   const [hours, setHours] = useState(false);
   const [menu, setMenu] = useState(false);
 
+  //toggle the visibility
   const handleHours = () => setHours((prev) => !prev);
   const handleMenu = () => setMenu((prev) => !prev);
 
@@ -19,8 +21,12 @@ function Restaurant(props) {
       </ul>
       <button onClick={handleHours}>Hours</button>
       <button onClick={handleMenu}>Menu</button>
+       {/* if the menu section is visible 
+       (menu state is true), display the menu */}
       {menu && (
         <ul>
+          {/* map through the restaurant menu array 
+          and display each item and its price */}
           {props.restaurant.menu.map((menuItem, index) => (
             <li key={index}>{`${menuItem.item} $${menuItem.price}`}</li>
           ))}
@@ -28,6 +34,8 @@ function Restaurant(props) {
       )}
       {hours && (
         <ul>
+          {/* map through the hours object 
+          and display the day and time */}
           {Object.entries(props.restaurant.hours).map(([day, time]) => (
             <li key={day}>
               <strong>{day.charAt(0).toUpperCase() + day.slice(1)}:</strong> {time}
