f349b16c83c57d64c59fe44da5669db45fdbc708
LauraV702 <dsfjsdfjlsdk@student.greenriver.edu>
2025-04-27 13:13:27 -0700

    Modified RestaurntsContainer.jsx to recieved and display the list of restaurants

--- usestate/src/components/RestaurantsContainer.jsx
+++ usestate/src/components/RestaurantsContainer.jsx
@@ -1,15 +1,13 @@
 import Restaurant from "./Restaurant";
 
-//the restaurnts component recieves a list of restaurnts as a prop
 function RestaurantsContainer(props) {
-  // console.log(props);
   return (
     <div className="restaurantContainer">
-      {/*iterating over the 'restaurants' array passed as a prop */}
-      {props.restaurants.map((restaurant) => (
-        //for each restaurant, rendering the Restaurant component and 
-        //passing the restaurant data as a prop
-        <Restaurant restaurant={restaurant} />
+      {/* iterate 'restaurants' array to be passed as prop */}
+      {props.restaurants.map((restaurant, index) => (
+        // for each restaurant, render 
+        //Restaurant component and pass the restaurant data as a prop
+        <Restaurant key={index} restaurant={restaurant} />
       ))}
     </div>
   );
